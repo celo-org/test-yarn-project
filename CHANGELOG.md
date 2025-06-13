42a2bf94424f398c7080b37f4158a3d889e6b42b
alvarof2 <fernandez.f.alvaro@gmail.com>
2025-06-13 11:16:39 +0200

    first commit

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+# @fernandezfalvaro/my-yarn-project
+
+## 1.0.23
+
+### Patch Changes
+
+- ss
---
+++ index.js
@@ -0,0 +1,4 @@
+// index.js
+import chalk from 'chalk';
+
+console.log(chalk.green('Hello, Yarn project test!'));
